5079fcf87b22e79e5cf31813a41994c04a85c285
Victor Guerra <vguerra@gmail.com>
2015-03-07 01:06:56 +0100

    Adding first version of the calculator layout. Placing logic that updates calculator display depending on buttons pressed.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    var typingANumber : Bool = false;
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if typingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            typingANumber = true
+         }
+        println("digit = \(digit)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
